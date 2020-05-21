9318d6dd9fa7e76503ddbbff160a1c737b61219c
Michael LeCompte <michaelslec98@gmail.com>
2020-05-20 22:06:07 -0700

    Added testing capabilities!!

---
+++ tests.c
@@ -0,0 +1,40 @@
+#include <check.h>
+#include "httpserver.h"
+
+START_TEST(our_first_test)
+{
+    ck_assert_int_eq(testingfunction(2), 2);
+    ck_assert_int_ne(testingfunction(2), 5);
+}
+END_TEST
+
+Suite * testing_suite(void)
+{
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("testing");
+
+    /* Core test case */
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, our_first_test);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+int main(void)
+{
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = testing_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
